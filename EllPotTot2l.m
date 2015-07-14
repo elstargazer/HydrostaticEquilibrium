734f70d9cf05e396fa552f4b4f049a5ea2eb713f
AntonErmakov <anton.i.ermakov@gmail.com>
2015-07-14 15:15:07 -0400

    deleted new lines

--- EllPotTot2l.m
+++ EllPotTot2l.m
@@ -1,17 +1,10 @@
 function U=EllPotTot2l(a1,c1,a2,c2,x,y,z,rho1,rho2,W)
 
-
 % Outer layer potential
-
-
-
 U=EllPot(a1,c1,x,y,z,rho1);
 
-
 % Inner layer potential
-
 U=U+EllPot(a2,c2,x,y,z,rho2-rho1);
 
 % Rotational potential
-
 U=U+RotPot(x,y,W);
