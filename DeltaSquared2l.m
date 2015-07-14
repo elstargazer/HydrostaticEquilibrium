95ef8b6e7f5aadbdf924d16cabcc6c577efab7bd
AntonErmakov <anton.i.ermakov@gmail.com>
2015-07-14 15:17:29 -0400

    broke long formula

--- DeltaSquared2l.m
+++ DeltaSquared2l.m
@@ -8,8 +8,13 @@ c1=r1/((1-f(1)).^(1/3))-f(1)*r1/((1-f(1)).^(1/3));
 a2=r2/((1-f(2)).^(1/3));
 c2=r2/((1-f(2)).^(1/3))-f(2)*r2/((1-f(2)).^(1/3));
 
+% [a1,b1,c1]=fr2abc(r1,fp1,fq1);
+% [a2,b2,c2]=fr2abc(r2,fp2,fq2);
+
 % 1 outer
 % 2 inner
 
-d2=(EllPotTot2l(a1,c1,a2,c2,0,0,c1,rho1,rho2,W)-EllPotTot2l(a1,c1,a2,c2,a1,0,0,rho1,rho2,W)).^2+...
-  (EllPotTot2l(a1,c1,a2,c2,0,0,c2,rho1,rho2,W)-EllPotTot2l(a1,c1,a2,c2,a2,0,0,rho1,rho2,W)).^2;
+d2=(EllPotTot2l(a1,c1,a2,c2,0,0,c1,rho1,rho2,W)-...
+    EllPotTot2l(a1,c1,a2,c2,a1,0,0,rho1,rho2,W)).^2+...
+    (EllPotTot2l(a1,c1,a2,c2,0,0,c2,rho1,rho2,W)-...
+    EllPotTot2l(a1,c1,a2,c2,a2,0,0,rho1,rho2,W)).^2;
