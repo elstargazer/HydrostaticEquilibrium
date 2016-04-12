00f3a45cf16230896c3956e9587fbcd0577c6d4f
AntonErmakov <anton.i.ermakov@gmail.com>
2016-04-12 17:42:20 -0400

    changed tol

--- HydrostaticStateExact2l.m
+++ HydrostaticStateExact2l.m
@@ -1,7 +1,7 @@
 function [fh,fval]=HydrostaticStateExact2l(r1,r2,T,rho1,rho2,f10,f20)
 
 [fh,fval]=fminsearch(@(f) DeltaSquared2l(f,r1,r2,T,rho1,rho2),[f10 f20],...
-    optimset('TolFun',1e-10));
+    optimset('TolFun',1e-15));
 
 
 
